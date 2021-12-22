8b1712a0534edb7c50a9f2c1b31b9e0d334c93ea
DanielPhilosoph <danielph9@gmail.com>
2021-12-22 12:12:37 +0200

    basic

---
+++ src/core/Page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Global weather status</h1>
+      <br />
+      <hr />
+      <div>Countries</div>
+    </div>
+  );
+}
---
+++ src/reducers/MainReducer.js
@@ -0,0 +1,9 @@
+export const MainReducer = (state, action) => {
+  switch (action.type) {
+    case "GET_COUNTRY":
+      break;
+
+    default:
+      break;
+  }
+};
